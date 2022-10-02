812c32f9c3dd900529103edf85262d3d8157dcdb
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-10-02 21:34:05 +0200

    perf: adjust permissions

--- init-s3/main.tf
+++ init-s3/main.tf
@@ -23,16 +23,32 @@ provider "aws" {
   secret_key = var.aws_secret_access_key
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state"
-  versioning {
-    enabled = true
+resource "aws_s3_bucket" "bucket_terraform_state" {
+  bucket = "tf-eks-cluster-terraform-state"
+}
+
+resource "aws_s3_bucket_versioning" "bucket_terraform_state_versioning" {
+  bucket = aws_s3_bucket.bucket_terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
   }
-  server_side_encryption_configuration {
-    rule {
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket_terraform_state_encryption" {
+  bucket = aws_s3_bucket.bucket_terraform_state.id
+  rule {
       apply_server_side_encryption_by_default {
         sse_algorithm = "AES256"
       }
     }
-  }
 }
+
+# resource "aws_dynamodb_table" "terraform_locks" {
+#   name         = "terraform-locks"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
