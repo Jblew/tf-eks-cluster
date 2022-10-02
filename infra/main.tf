bfe2bc77e83df7d09f7378c03a6a3692247cf8fe
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-10-02 21:53:59 +0200

    chore: setup infra

---
+++ infra/main.tf
@@ -0,0 +1,24 @@
+variable "aws_access_key_id" {
+  type      = string
+  sensitive = true
+}
+variable "aws_secret_access_key" {
+  type      = string
+  sensitive = true
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+}
