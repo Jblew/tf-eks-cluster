278c3257d179422a5b8ae25217581914d2568eaa
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-10-02 21:11:09 +0200

    feat: init s3

---
+++ init-s3/main.tf
@@ -0,0 +1,38 @@
+variable "aws_access_key_id" {
+  type      = string
+  sensitive = true
+}
+variable "aws_secret_access_key" {
+  type      = string
+  sensitive = true
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state"
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
